aaf7db960d584f90450bf734f49993afc920b08b
Saurab Gami <saurabgami977@gmail.com>
2021-04-13 12:41:47 +0545

    vector implemented in guess game

--- Game-1/a.cpp
+++ Game-1/a.cpp
@@ -1,46 +1,72 @@
-#include<iostream>
-#include<string>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
-void playGame(){
+void print_vector(vector<int> array)
+{
+    for (int i = 0; i < array.size(); i++)
+    {
+        cout << array[i] << '\t';
+    }
+
+    cout << endl;
+}
+
+void playGame()
+{
+
+    vector<int> guesses = {};
+
     int random = rand() % 251;
-    cout<<random <<endl;
-    cout<<"Enter the guess number: ";
-    while(true){
+    cout << random << endl;
+    cout << "Enter the guess number: ";
+    while (true)
+    {
         int guess;
         cin >> guess;
-        if(guess == random){
-            cout<<"You Win!!!\n";
+        guesses.push_back(guess);
+        if (guess == random)
+        {
+            cout << "You Win!!!\n";
             break;
-        }else if(guess < random){
-            cout<<"Its too low!!!\n";
-            cout<<"Enter the guess number again: ";
-        }else{
-            cout<<"Its too High!!!\n";
-            cout<<"Enter the guess number again: ";
+        }
+        else if (guess < random)
+        {
+            cout << "Its too low!!!\n";
+            cout << "Enter the guess number again: ";
+        }
+        else
+        {
+            cout << "Its too High!!!\n";
+            cout << "Enter the guess number again: ";
         }
     }
+    print_vector(guesses);
 };
 
-int main (){
+int main()
+{
     srand(time(NULL));
     int choice;
-    do{
-    cout<<"0. Quit" <<endl << "1. Play Game" <<endl;
-    cout<<"Enter your choice: ";
-    cin>>choice;
-    switch (choice)
+    do
     {
-    case 0:
-        cout<<"Thanks! You Quit." <<endl;
-        break;
-    default:
-        playGame();
-        break;
-    }
-    }while(choice != 0);
+        cout << "0. Quit" << endl
+             << "1. Play Game" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 0:
+            cout << "Thanks! You Quit." << endl;
+            break;
+        default:
+            playGame();
+            break;
+        }
+    } while (choice != 0);
     return 0;
 }
---
+++ Template__Array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    array<int, 5> ages = {1, 2, 3};
+    cout << ages[0];
+    return 0;
+}
