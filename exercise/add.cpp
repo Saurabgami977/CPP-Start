11d2fc21162aa21dbe95439abed4f3bbdaf563d8
Saurab Gami <saurabgami977@gmail.com>
2021-04-22 08:17:40 +0545

    add function

---
+++ exercise/add.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void addition(int a, int b)
+{
+    cout << a + b << endl;
+};
+
+int main()
+{
+    addition(4, 6);
+    addition(4, 8);
+    addition(4, 9);
+}
