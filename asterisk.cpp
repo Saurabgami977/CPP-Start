b789ac750eedf7da3f421e5a5a7dafa0c5e5f3a7
Saurab Gami <saurabgami977@gmail.com>
2021-04-22 08:51:13 +0545

    printing variables

--- asterisk.cpp
+++ asterisk.cpp
@@ -2,18 +2,29 @@
 
 using namespace std;
 
-void print(int a)
-{
-    for (int j = 0; j < a; j++)
-        cout << '*';
-    cout << endl;
-};
+// void print(int a)
+// {
+//     for (int j = 0; j < a; j++)
+//         cout << '*';
+//     cout << endl;
+// };
 
 int main()
 {
-    print(4);
-    print(5);
-    print(6);
-    print(7);
-    print(8);
+    // print(4);
+    // print(5);
+    // print(6);
+    // print(7);
+    // print(8);
+
+    int x;
+    int y;
+    float z;
+    x = 10;
+    y = 15;
+    z = 12.6;
+
+    cout << "x=" << x << "\t"
+         << "y= " << y << "\t"
+         << "z=" << z;
 }
