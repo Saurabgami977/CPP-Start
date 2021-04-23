600799aa36cd090602f958a1e6a4acb4806dd042
Saurab Gami <saurabgami977@gmail.com>
2021-04-23 11:08:17 +0545

    progtam to find square root

---
+++ exercise/squareroot.cpp
@@ -0,0 +1,13 @@
+// Square Root of Number program in C++
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int number = 2500;
+    int squareRoot = sqrt(number);
+    cout << squareRoot << endl;
+}
